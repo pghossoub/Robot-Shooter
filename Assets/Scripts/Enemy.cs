ab18c363d1351e239476ddc0e3e2637449cdf6c9
Pierre Ghossoub <pghossoub@hotmail.com>
2019-03-05 20:31:11 +0100

    enemy bounce when hit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,31 +11,60 @@ public class Enemy : MonoBehaviour {
 	public GameObject legs;
 	public GameObject deathExplosion;
 
+
 	//private because component attached to enemy
 	private Animator animatorLegs;
 	private AudioSource shotSound;
 	private Transform tr;
 	private Rigidbody2D rb;
+	private GameObject player;
+	private Transform trPlayer;
 
 	void Start () 
 	{
 		shotSound = GetComponent<AudioSource> ();
 		tr = GetComponent<Transform> ();
 		rb = GetComponent<Rigidbody2D> ();
+
+		player = GameObject.FindWithTag("Player");
+		trPlayer = player.GetComponent<Transform> ();
 	}
 
-	public void LosePv (float damage, Transform bulletTransform)
+	void FixedUpdate()
+	{
+		//Vector3 direction = tr.position - trPlayer.position;
+		//Debug.Log ("direction: "+ direction);
+		//tr.rotation = Quaternion.LookRotation (direction, Vector3.forward);
+		//Debug.Log(tr.position - trPlayer.position);
+
+		//Quaternion enemyRotation = Quaternion.LookRotation (tr.position - trPlayer.position, Vector3.forward);
+		//tr.rotation = enemyRotation;
+
+		//tr.eulerAngles = new Vector3 (0, 0, tr.eulerAngles.z);
+		//rb.angularVelocity = 0;
+
+		//Quaternion PlayerRotation = Quaternion.LookRotation (tr.position - mousePosition, Vector3.forward);
+		//tr.rotation = PlayerRotation;
+
+		Vector3 direction = (trPlayer.position - tr.position).normalized;
+		Quaternion lookRotation = Quaternion.LookRotation(direction, Vector3.forward);
+		tr.rotation = Quaternion.Slerp(tr.rotation, lookRotation, Time.deltaTime * 100);
+		tr.eulerAngles = new Vector3 (0, 0, tr.eulerAngles.z);
+	}
+
+	public void LosePv (float damage)
 	{
 		pv = pv - damage;
-		//Vector3 movement = new Vector3 (bulletTransform.rotation.eulerAngles.z, 
-		//								bulletTransform.rotation.eulerAngles.z, 
-		//								0);
-		//Vector3 movement = bulletTransform.rotation * Vector3.forward;
-		//Debug.Log ("movement:" + movement);
-		//rb.AddForce(movement * 1);
 		if (pv <= 0) {
 			Instantiate (deathExplosion, tr.position, tr.rotation);
 			Destroy (gameObject);
 		}
 	}
+
+	public void bounceOnImpact(Transform bulletTransform)
+	{
+		Vector3 movement = bulletTransform.rotation * Vector3.up;
+		rb.AddForce(movement * 100);
+	}
 }
+	
--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -7,7 +7,8 @@ public class PlayerBullet : Projectile {
 	protected override void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.CompareTag("Enemy")){
-			other.GetComponent<Enemy>().LosePv(damage, tr);
+			other.GetComponent<Enemy>().LosePv(damage);
+			other.GetComponent<Enemy>().bounceOnImpact(tr);
 		}
 
 		base.OnTriggerEnter2D(other);
