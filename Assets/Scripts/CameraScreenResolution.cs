d70261f504280a6d4c6663ce0901117578842533
Pierre Ghossoub <pghossoub@hotmail.com>
2019-05-22 22:25:23 +0200

    V1.1 : Screen Resolution fixed

---
+++ Assets/Scripts/CameraScreenResolution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScreenResolution : MonoBehaviour {
+
+	public bool maintainWidth = true;
+
+	private float defaultWidth;
+	private float defaultHeight;
+
+	Vector3 CameraPos;
+
+	void Start () 
+	{
+		CameraPos = Camera.main.transform.position;
+		defaultHeight = Camera.main.orthographicSize;
+		defaultWidth = Camera.main.orthographicSize * Camera.main.aspect;
+	}
+	
+	void Update () 
+	{
+		if (maintainWidth) {
+			Camera.main.orthographicSize = defaultWidth / Camera.main.aspect;
+			Camera.main.transform.position = new Vector3(CameraPos.x, -1*(defaultHeight-Camera.main.orthographicSize), CameraPos.z);
+
+		}
+	}
+}
